efb44dec3c18854d31e038727405127fe6c67e18
carjiso <cjimenez@indigitalsolutions.com>
2024-11-24 03:37:57 -0500

    fix webhook whatsapp

--- src/webhook/WhatsappWebhook.py
+++ src/webhook/WhatsappWebhook.py
@@ -1,22 +1,42 @@
 from fastapi import APIRouter
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
+from fastapi import FastAPI, Request, HTTPException
+# from fastapi.responses import JSONResponse
+# from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse, PlainTextResponse
+
+VERIFY_TOKEN = "HolaNovato"
 
 class WhatsappWebhook():
 
     whatsappWebhook = APIRouter()
 
-    @whatsappWebhook.get('/webhook/{args}', tags=['bot-whatsapp'], response_model=dict, status_code=200)
-    def sendMessage(args):
-        print("args: ")
-        print(type(args))
-        print(args)
-        """
-        Send Message to whatsapp\n
-        parameter phoneTo\n
-        parameter message
-        """
-        # botWhatsappService.sendMessageWhatsapp(phoneTo)
-        return JSONResponse(status_code=200, content=jsonable_encoder("Hello"))
+    @whatsappWebhook.api_route("/webhook/", methods=["POST", "GET"])
+    async def webhook_whatsapp(request: Request):
+        # SI HAY DATOS RECIBIDOS VIA GET
+        print("request", request)
+        if request.method == "GET":
+            params = request.query_params
+            # SI EL TOKEN ES IGUAL AL QUE RECIBIMOS
+            if params.get('hub.verify_token') == "HolaNovato":
+                # ESCRIBIMOS EN EL NAVEGADOR EL VALOR DEL RETO RECIBIDO DESDE FACEBOOK
+                return PlainTextResponse(params.get('hub.challenge'))
+            else:
+                # SI NO SON IGUALES RETORNAMOS UN MENSAJE DE ERROR
+                raise HTTPException(status_code=403, detail="Error de autentificación.")
 
+        # SI LA PETICIÓN ES POST, RECIBIMOS LOS DATOS ENVIADOS VIA JSON
+        data = await request.json()
+        try:
+            # EXTRAEMOS EL NÚMERO DE TELÉFONO Y EL MENSAJE
+            telefono = data['entry'][0]['changes'][0]['value']['messages'][0]['from']
+            mensaje_texto = data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
+            mensaje = f"Telefono: {telefono} | Mensaje: {mensaje_texto}"
+            print("mensaje: ", mensaje)
+            # ESCRIBIMOS EL NÚMERO DE TELÉFONO Y EL MENSAJE EN EL ARCHIVO TEXTO
+            # with open("texto.txt", "w") as f:
+            #     f.write(mensaje)
 
+            # RETORNAMOS EL STATUS EN UN JSON
+            return JSONResponse(content={"status": "success"}, status_code=200)
+        except KeyError as e:
+            raise HTTPException(status_code=400, detail=f"Datos incompletos: {e}")
