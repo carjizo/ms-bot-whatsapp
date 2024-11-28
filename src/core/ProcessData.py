5b037426071a6a2d2a483a5fc1168094ada576a4
carjiso <cjimenez@indigitalsolutions.com>
2024-11-27 22:58:55 -0500

    fix print

--- src/core/ProcessData.py
+++ src/core/ProcessData.py
@@ -3,15 +3,14 @@ from src.core.Constants import Constants
 class ProcessData():
     def getData(data: dict) -> dict:
         response = {}
-        print("self.data", data, type(data))
         phone = data['entry'][0]['changes'][0]['value']['messages'][0]['from']
-        type = data['entry'][0]['changes'][0]['value']['messages'][0]
+        type_event = data['entry'][0]['changes'][0]['value']['messages'][0]
         response["phone"] = phone
         response["type"] = type
-        if type == Constants.TYPE_TEXT:
+        if type_event == Constants.TYPE_TEXT:
             message_text = data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
             response["message_text"] = message_text
-        if type == Constants.TYPE_DOCUMENT:
+        if type_event == Constants.TYPE_DOCUMENT:
             mime_type = data['entry'][0]['changes'][0]['value']['messages'][0]['document']['mime_type']
             id_document = data['entry'][0]['changes'][0]['value']['messages'][0]['document']['id']
             response["mime_type"] = mime_type
