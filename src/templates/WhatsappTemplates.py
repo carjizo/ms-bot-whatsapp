fcc561448a7f393261a67912cb00156f7d4be347
carjiso <cjimenez@indigitalsolutions.com>
2024-12-06 15:03:15 -0500

    fix components

--- src/templates/WhatsappTemplates.py
+++ src/templates/WhatsappTemplates.py
@@ -8,6 +8,15 @@ class WhatsappTemplates:
             "language": {"code": "es"},
             "components": [
                 {
+                    "type": "header",
+                    "parameters": [
+                        {
+                            "type": "image",
+                            "text": f"https://www.google.com/imgres?q=calculadora%20de%20distancia%20entre%20dos%20puntos&imgurl=https%3A%2F%2Fi0.wp.com%2Flasmatesfaciles.com%2Fwp-content%2Fuploads%2F2023%2F03%2Fimage-7.png%3Fresize%3D840%252C806%26ssl%3D1&imgrefurl=https%3A%2F%2Flasmatesfaciles.com%2F2023%2F03%2F09%2Fdistancia-entre-dos-puntos%2F&docid=wa2JHx97ooX0TM&tbnid=Sws5FDDPjK7PfM&vet=12ahUKEwi36-XP4JKKAxWPCrkGHU_QEnAQM3oECFIQAA..i&w=840&h=806&hcb=2&ved=2ahUKEwi36-XP4JKKAxWPCrkGHU_QEnAQM3oECFIQAA"
+                        }
+                    ]
+                },
+                {
                     "type": "body",
                     "parameters": [
                         {
