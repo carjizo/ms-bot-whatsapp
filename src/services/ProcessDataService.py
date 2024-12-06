650bec3de16cca791ee15ccdabeeae85c7b6be13
carjiso <cjimenez@indigitalsolutions.com>
2024-12-06 17:28:59 -0500

    add exceptions

--- src/services/ProcessDataService.py
+++ src/services/ProcessDataService.py
@@ -14,26 +14,30 @@ class ProcessData():
         response["phone"] = phone
         response["type"] = type_event
         
-        if type_event == Constants.TYPE_TEXT:
-            print("TYPE_TEXT")
-            message_text = dataMessage['text']['body']
-            response["message_text"] = message_text
-            TypeTextService.processText(phone, message_text)
-        elif type_event == Constants.TYPE_BUTTON:
-            print("TYPE_BUTTON")
-            message_text = dataMessage['button']['text']
-            response["message_text"] = message_text
-            TypeButtonService.processButton(phone, message_text)
-        elif type_event == Constants.TYPE_DOCUMENT:
-            mime_type = dataMessage['document']['mime_type']
-            id_document = dataMessage['document']['id']
-            response["mime_type"] = mime_type
-            response["id_document"] = id_document
-        elif type_event == Constants.TYPE_IMAGE:
-            mime_type = dataMessage['image']['mime_type']
-            id_document = dataMessage['image']['id']
-            response["mime_type"] = mime_type
-            response["id_document"] = id_document
-            TypeDocumentService.processDocument(mime_type, id_document)
-        
+        try:
+            if type_event == Constants.TYPE_TEXT:
+                print("TYPE_TEXT")
+                message_text = dataMessage['text']['body']
+                response["message_text"] = message_text
+                TypeTextService.processText(phone, message_text)
+            elif type_event == Constants.TYPE_BUTTON:
+                print("TYPE_BUTTON")
+                message_text = dataMessage['button']['text']
+                response["message_text"] = message_text
+                TypeButtonService.processButton(phone, message_text)
+            elif type_event == Constants.TYPE_DOCUMENT:
+                mime_type = dataMessage['document']['mime_type']
+                id_document = dataMessage['document']['id']
+                response["mime_type"] = mime_type
+                response["id_document"] = id_document
+            elif type_event == Constants.TYPE_IMAGE:
+                mime_type = dataMessage['image']['mime_type']
+                id_document = dataMessage['image']['id']
+                response["mime_type"] = mime_type
+                response["id_document"] = id_document
+                TypeDocumentService.processDocument(mime_type, id_document)
+        except KeyError as k_err:
+            print("KeyError", str(k_err))
+        except Exception as err:
+            print("Exception", str(err))
         return response
