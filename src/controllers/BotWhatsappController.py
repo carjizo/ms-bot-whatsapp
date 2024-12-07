196388ee9c55a9f0a929036fe93d7bfeebfb527d
carjiso <cjimenez@indigitalsolutions.com>
2024-12-07 00:42:56 -0500

    fix initialize BotWhatsappService

--- src/controllers/BotWhatsappController.py
+++ src/controllers/BotWhatsappController.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 
-botWhatsappService = BotWhatsappService()
+# botWhatsappService = BotWhatsappService()
 
 class BotWhatsappController():
 
