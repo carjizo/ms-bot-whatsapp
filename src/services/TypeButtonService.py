d1b26c620bf59c06730484edebca85e45d28d297
carjiso <cjimenez@indigitalsolutions.com>
2024-12-09 03:14:40 -0500

    fix resumen_presupuesto

--- src/services/TypeButtonService.py
+++ src/services/TypeButtonService.py
@@ -13,12 +13,12 @@ class TypeButtonService():
         response = {}
         self.message_text = self.message_text.lower()
         print("message_text_lower", self.message_text)
-        isBudgetFLow: bool = False 
+        isBudgetFLow: bool = False
+        botWhatsappService = BotWhatsappService(self.phone, self.message_text, self.idWa, self.fullName)
         if (self.message_text in [Constants.BUTTON_REGISTRAR_GASTO, Constants.BUTTON_REGISTRAR_INGRESO]): 
             isBudgetFLow = True
         if isBudgetFLow:
-            botWhatsappService = BotWhatsappService(self.phone, self.message_text, self.idWa, self.fullName)
             response = botWhatsappService.sendMessageInputAmount()
-        if self.message_text == Constants.BUTTON_RESUMEN_PRESUPUESTO:
+        if self.message_text == Constants.BUTTON_RESUMEN_PRESUPUESTO:            
             response = botWhatsappService.sendMessageInputBudgetSummary()
         return response
